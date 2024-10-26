3b2a68443cb66dbddb225f7099ee87b9403db113
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-10-26 18:57:34 +0300

    complete loops lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ["Kelvin", "Mark", "Jane"];
+let event = "birthday";
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+};
+
+function countDown(num) {
+    let i = 0;
+    while (num > i) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+};
